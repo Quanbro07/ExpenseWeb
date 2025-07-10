0f6646a7fb344b3e7fcc49496b0997c3953f759c
anhJ04 <hs.nguyenthanhtrong@gmail.com>
2025-07-10 11:47:30 +0700

    Update tên biến
    user.name=user.userName

--- frontend/src/UserCard.jsx
+++ frontend/src/UserCard.jsx
@@ -3,7 +3,7 @@ import avatar from './avatar.svg';
 import qrcode from './qrcode.png';
 
 export default function UserCard({ user }) {
-    console.log(user);
+  console.log(user);
 
   if (!user) {
     return <div className="userCard">Đang tải thông tin người dùng...</div>;
@@ -14,7 +14,7 @@ export default function UserCard({ user }) {
       <div className="glass">
         <img src={avatar} alt="Avatar" className="avatar" />
         <div className="information">
-          <div className="name">{user.username || user.name || "Không tên"}</div>
+          <div className="name">{user.userName || "Không tên"}</div>
           <div className="id">ID: {user.id}</div>
           <div className="balance">Số dư: {user.balance ?? 0} VNĐ</div>
         </div>
